d0328e52a93b32a7547f854e66b89b2d29b3a684
Diego B Morais <128264029+diegobmorais@users.noreply.github.com>
2023-10-26 19:39:19 -0300

    Delete api_pets directory
    old

--- api_pets/config.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-date_default_timezone_set('America/Sao_Paulo');
-
-header("Content-Type: application/json"); // a aplicação retorna json
-header("Access-Control-Allow-Origin: *"); // vai aceitar requisições de todas origens
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); // habilita métodos
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
